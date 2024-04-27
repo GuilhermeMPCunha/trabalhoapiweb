4b7ce1001aca6770076edcf49bd1987dc4acd5c8
GuilhermeMPCunha <guilhermecunha10@hotmail.com>
2024-04-27 11:40:08 -0300

    Spring Boot Project

---
+++ src/main/java/com/trabalhoapi/trabalhoapiweb/TrabalhoapiwebApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhoapi.trabalhoapiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoapiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoapiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalhoapi/trabalhoapiweb/TrabalhoapiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhoapi.trabalhoapiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoapiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
